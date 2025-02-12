e41dd507e38651e877707fe5065c661a650bcd85
Kribean <caraibe.simulation@gmail.com>
2025-02-12 20:06:31 +0100

    correct component

--- src/app/utilisateurs/page.js
+++ src/app/utilisateurs/page.js
@@ -1,9 +1,5 @@
 "use client"
-import BusinessRegistration from "@/components/business-registration";
-import HomePage from "@/components/landing-page-auth";
-import AppointmentsPage from "@/components/table-management";
 import UserDashboard from "@/components/user/UserDashboard";
-import Image from "next/image";
 
 export default function Home() {
   return (
